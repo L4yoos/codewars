5257eff32c321c78513a6b818503227410e586d9
L4yoos <daleckikonrad1@gmail.com>
2026-06-18 11:56:52 +0200

    CountingSheep, RemoveStringSpaces, YouCantCodeUnderPressure (day 26)

---
+++ 8_Kyu/RemoveStringSpaces.java
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+// Examples (Input -> Output):
+//  "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+//  "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+//  "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+public class Kata {
+    // 1st Solution
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+    // 2nd Solution
+    public static String noSpace(final String x) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = x.toCharArray();
+        for (char c : chars) {
+            if (c != ' ') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
