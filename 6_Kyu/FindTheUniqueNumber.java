91e60ee4d26a27dc47b2a89c250f677b4dd1638a
L4yoos <daleckikonrad1@gmail.com>
2026-06-29 10:21:33 +0200

    IsItEven, ReturnNegative, FindMaximumAndMinimumValuesOfAList, FindTheUniqueNumber(day 36)

---
+++ 6_Kyu/FindTheUniqueNumber.java
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+//
+// Kata.findUniq(new double[]{ 1, 1, 1, 2, 1, 1 }); // => 2
+// Kata.findUniq(new double[]{ 0, 0, 0.55, 0, 0 }); // => 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+//
+// The tests contain some very huge arrays, so think about performance.
+//
+// This is the first kata in series:
+//
+// Find the unique number (this kata)
+// Find the unique string
+// Find The Unique
+
+public class Kata {
+    public static double findUniq(double arr[]) {
+        Set<Double> seen = new HashSet<>();
+        Set<Double> duplicates = new HashSet<>();
+        for (double num : arr) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+        for (double num : seen) {
+            if (!duplicates.contains(num)) {
+                return num;
+            }
+        }
+        return 0.0;
+    }
+}
