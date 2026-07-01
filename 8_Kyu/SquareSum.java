40bea45b492f9f9cfccea2eb41292d5040400b1c
L4yoos <daleckikonrad1@gmail.com>
2026-07-01 11:15:57 +0200

    QuarterOfTheYear, SquareSum, OnesAndZeros, FindTheStrayNumber (day 38)

---
+++ 8_Kyu/SquareSum.java
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/515e271a311df0350d00000f
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//
+// For example, for [1, 2, 2] it should return 9 because
+
+public class Kata {
+    public static int squareSum(int[] n) {
+        int sum = 0;
+        for (int num : n) {
+            sum = sum + (num * num);
+        }
+        return sum;
+    }
+}
