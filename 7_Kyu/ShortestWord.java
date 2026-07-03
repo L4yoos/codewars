27b41435162e95d16c813858f15ce785e16b979e
L4yoos <daleckikonrad1@gmail.com>
2026-07-03 12:36:22 +0200

    A new solutions for: VowelCount, ShortestWord (day 40)

--- 7_Kyu/ShortestWord.java
+++ 7_Kyu/ShortestWord.java
@@ -5,6 +5,7 @@
 //String will never be empty and you do not need to account for different data types.
 
 public class Kata {
+    //1st Solution
     public static int findShort(String s) {
         String[] words = s.split(" ");
         int min = 999;
@@ -14,5 +15,47 @@ public class Kata {
             }
         }
         return min;
+    }
+    //2nd Solution
+    public static int findShort(String s) {
+        String[] parts = s.split(" ");
+        int length = parts.length;
+
+        int shortest = Integer.MAX_VALUE;
+        for (int i = 0; i < length; i++) {
+            int partOne = parts[i].length();
+
+            if (partOne < shortest) {
+                shortest = partOne;
+            }
+        }
+        return shortest;
+    }
+    //3nd Solution, O(1) space complexity
+    public static int findShort(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        int shortest = Integer.MAX_VALUE;
+        int currentWordLength = 0;
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == ' ') {
+                if (currentWordLength < shortest) {
+                    shortest = currentWordLength;
+                }
+                currentWordLength = 0;
+            } else {
+                currentWordLength++;
+            }
+        }
+
+        if (currentWordLength > 0 && currentWordLength < shortest) {
+            shortest = currentWordLength;
+        }
+
+        return shortest;
     }
 }
--- 7_Kyu/VowelCount.java
+++ 7_Kyu/VowelCount.java
@@ -28,4 +28,48 @@ public class Vowels {
         }
         return count;
     }
+    //3nd Solution
+    public static int getCount(String str) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        int counter = 0;
+        for (char c : str.toCharArray()) {
+            if (vowels.contains(c)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    //4th Solution
+    public static int getCount(String str) {
+        int l = str.length();
+        str = str.replaceAll("a|e|i|o|u", "");
+        int secL = str.length();
+        return l - secL;
+    }
+    //5th Solution
+    public static int getCount(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        int count = 0;
+        int length = str.length();
+
+        for (int i = 0; i < length; i++) {
+            char c = str.charAt(i);
+
+            switch (c) {
+                case 'a', 'e', 'i', 'o', 'u' -> count++;
+                default -> {}
+            }
+        }
+
+        return count;
+    }
 }
