27ec400f59384f329446eaeaf9ff2370be1f1e1b
L4yoos <daleckikonrad1@gmail.com>
2026-07-12 16:00:24 +0200

    AllStarCodeChallenge18, GetCharacterFromASCIIValue, SmallEnough (day 48)

---
+++ 8_Kyu/GetCharacterFromASCIIValue.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c
+
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+//
+// Example:
+//
+// 65 --> 'A'
+// 7 --> 'a'
+// 48 --> '0
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+public class Ascii {
+    public static char getChar(int c) {
+        return (char) c;
+    }
+}
