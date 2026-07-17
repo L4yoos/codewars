3147285dcb78fa40f153b264a0a0a8828a20f3c1
L4yoos <daleckikonrad1@gmail.com>
2026-07-17 12:19:33 +0200

    Fix: ExclamationMarksSeriesReplaceAllVowelToExclamationMarkInTheSentence, TotalAmountOfPoints, JadenCasingStrings (day 53)

--- 8_Kyu/ExclamationMarksSeriesReplaceAllVowelToExclamationMarkInTheSentence.java
+++ 8_Kyu/ExclamationMarksSeriesReplaceAllVowelToExclamationMarkInTheSentence.java
@@ -1,7 +1,20 @@
+// https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+//
+// Examples
+// "Hi!" --> "H!!"
+// "!Hi! Hi!" --> "!H!! H!!"
+// "aeiou" --> "!!!!!"
+// "ABCDE" --> "!BCD!"
+
 public class Solution {
+    //1st Solution
     public static String replace(final String s) {
         return s.replaceAll("[aeiouAEIOU]", "!");
     }
+    //2nd Solution
     public static String replace(final String s) {
         return s.replaceAll("a|A|e|E|I|i|O|o|U|u", "!");
     }
