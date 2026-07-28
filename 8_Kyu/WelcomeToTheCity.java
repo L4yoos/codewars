5908dddb8e4ecb60978993c251fbf36f9d1e288f
L4yoos <daleckikonrad1@gmail.com>
2026-07-28 19:03:18 +0200

    WelcomeToTheCity, BeginnerSeriesSumOfNumbers (day 64)

---
+++ 8_Kyu/WelcomeToTheCity.java
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4
+
+// Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+//
+// Example:
+//
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+public class Hello {
+    public String sayHello(String [] name, String city, String state){
+        if (name.length > 2) {
+            return String.format("Hello, %s %s %s! Welcome to %s, %s!", name[0], name[1], name[2], city, state);
+        }
+        return String.format("Hello, %s %s! Welcome to %s, %s!", name[0], name[1], city, state);
+    }
+}
