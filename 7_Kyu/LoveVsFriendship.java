c314a0b4140ad0ca928449bf9e09e2dc06c9e4c1
L4yoos <daleckikonrad1@gmail.com>
2026-08-03 15:32:27 +0200

    MultiplesOf3Or5, LoveVsFriendship (day 70)

---
+++ 7_Kyu/LoveVsFriendship.java
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/59706036f6e5d1e22d000016
+
+// If　a = 1, b = 2, c = 3 ... z = 26
+//
+// Then l + o + v + e = 54
+//
+// and f + r + i + e + n + d + s + h + i + p = 108
+//
+// So friendship is twice as strong as love :-)
+//
+// Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+//
+// The input will always be made of only lowercase letters and will never be empty.
+
+public class LoveVsFriendship {
+    public static int wordsToMarks (String text) {
+        int sum = 0;
+        for (char c : text.toCharArray()) {
+            sum += c - 'a' + 1;
+        }
+        return sum;
+    }
+}
