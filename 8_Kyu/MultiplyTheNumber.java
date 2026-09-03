f1b2eb742f9c6d3a0da281800c50dd1d864ab6de
L4yoos <daleckikonrad1@gmail.com>
2026-09-03 10:55:28 +0200

    MultiplyTheNumber (day 101)

---
+++ 8_Kyu/MultiplyTheNumber.java
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/5708f682c69b48047b000e07
+
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+//
+//   3 -->    15  (  3 * 5¹)
+//  10 -->   250  ( 10 * 5²)
+// 200 --> 25000  (200 * 5³)
+//   0 -->     0  (  0 * 5¹)
+//  -3 -->   -15  ( -3 * 5¹)
+public class Kata {
+    public static int multiply(int number) {
+        int digits = 0;
+        int dumpNumber = number;
+        while (dumpNumber != 0) {
+            dumpNumber /= 10;
+            digits++;
+        }
+        return  number * (int) Math.pow(5, digits);
+    }
+}
