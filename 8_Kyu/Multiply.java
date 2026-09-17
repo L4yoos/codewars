b4b82dcaa919355abef5307fb97ecae2bd36b188
L4yoos <daleckikonrad1@gmail.com>
2026-09-17 12:21:07 +0200

    ThinkingAndTesting, Multiply (day 114)

---
+++ 8_Kyu/Multiply.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+// This code does not execute properly. Try to figure out why.
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
